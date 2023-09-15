6e1adf163eacb0c8f307caadd9688499ae75e7dd
scup0o <haiphung0015@gmail.com>
2023-09-15 10:47:47 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -12,5 +13,18 @@ app.get("/",(req, res) =>{
 });
 
 app.use("/api/contacts", contactsRouter);
+
+//404 
+app.use((req, res, next) => {
+    //res.send('Something broke!')
+    return next(new ApiError(404, "Resource not found"));
+});
+
+// xử lý lỗi tập trung
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
